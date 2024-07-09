9486133a88d63cc6334cc48ec91b681ab292f9c8
Thiago-dcode <contact.thiago.ferreira@gmail.com>
2024-07-09 20:45:38 +0200

    start implementing array

---
+++ src/app/data-structures/linear/array/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Array from '@/entities/data-structures/linear/array/View'
+function ArrayPage() {
+  return (
+    <>
+    <Array/>
+    </>
+  )
+}
+
+export default ArrayPage
